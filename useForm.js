e371404121b1f796490c101cfb44a9b31eba56f1
Stephania Zambrano <stezague88@gmail.com>
2021-11-28 17:52:31 -0500

    initial

---
+++ useForm.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+//Este hook me permite leer todo lo que tengo en un input
+
+export const useForm = ( initialState = {} ) => {
+    
+    const [values, setValues] = useState(initialState);
+
+    const reset = () => {
+        setValues( initialState );
+    }
+
+
+    const handleInputChange = ({ target }) => {
+
+        setValues({
+            ...values,
+            [ target.name ]: target.value
+        });
+
+    }
+
+    return [ values, handleInputChange, reset ];
+
+}
